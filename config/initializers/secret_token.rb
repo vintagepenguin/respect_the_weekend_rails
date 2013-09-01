8a98519200673e5c92569bf253d4b4a3b9670c05
dan jones <danwjones22@gmail.com>
2013-09-01 16:17:19 -0500

    load skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RespectTheWeekend::Application.config.secret_token = 'b5b08556dc6d2fafa792b0e3db620599786aca4d151344ad9e616a2d7f0edf11aca19ee9e6fd2c4595a60e022f34665cf9e1554f993e11585e9ab70a945bb800'
